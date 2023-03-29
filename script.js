aff2d666464c84d4d5c46772bc9c1f5ec94fbcb4
Sean Ebenmelu <40689266+sebems@users.noreply.github.com>
2023-03-28 23:53:43 -0400

    added color choice

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-var onBTN = document.getElementById("onButton")
-var offBTN = document.getElementById("offButton")
-var body = document.getElementById('body')
+var onBTN = document.getElementById("onButton");
+var offBTN = document.getElementById("offButton");
+var inColor = document.getElementById("light_color");
+var body = document.getElementById("body");
 
-onBTN.addEventListener("click", function(){
+onBTN.addEventListener("click", function () {
   // forwards holds the last state of the animation
-  body.style.animation = "lightOn 2s linear forwards"
-})
+  body.style.animation = "lightOn 2s linear forwards";
+});
 
-offBTN.addEventListener("click", function(){
-  body.style.animation = "lightOff 0.5s linear forwards"
-})
+offBTN.addEventListener("click", function () {
+  body.style.animation = "lightOff 0.5s linear forwards";
+});
+
+inColor.addEventListener("input", function () {
+  body.style.backgroundColor = inColor.innerText;
+});
