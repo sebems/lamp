33582901ca0d19fba18c3c37c592d5e1b08b8314
Sean Ebenmelu <jindusean@gmail.com>
2023-11-13 19:30:07 -0500

    made a single light-switch and made the light the color picker choice

--- script.js
+++ script.js
@@ -1,17 +1,47 @@
-var onBTN = document.getElementById("onButton");
-var offBTN = document.getElementById("offButton");
+var light_switch = document.getElementById("switch")
 var inColor = document.getElementById("light_color");
 var body = document.getElementById("body");
 
-onBTN.addEventListener("click", function () {
+var iconImg = document.getElementById("icon")
+
+document.getElementById("color_text").innerText = inColor.value
+
+// condition for light flip flop
+var isOn = false;
+
+light_switch.addEventListener("click", function () {
+  isOn = !isOn;
+
+  if (isOn){
+    turnOn();
+  }
+  else {
+    turnOff();
+  }
+})
+
+turnOn = () => {
+  // change icon
+  iconImg.href = "light_on.png";
+
   // forwards holds the last state of the animation
   body.style.animation = "lightOn 2s linear forwards";
-});
+};
 
-offBTN.addEventListener("click", function () {
+turnOff = () => {
+
+  iconImg.href = "light_off.png";
+
   body.style.animation = "lightOff 0.5s linear forwards";
-});
+}
 
+// detects color picker change and light change
 inColor.addEventListener("input", function () {
-  body.style.backgroundColor = inColor.innerText;
+  console.log(inColor.value);
+  document.documentElement.style.setProperty('--light_color', inColor.value);
+
+  // color text
+
+  document.getElementById("color_text").innerText = inColor.value
+
 });
